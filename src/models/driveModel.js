d208b8af9024a0bf8331b1182a4d69bc445702e6
Laura Warr <laura.warr@mimik.com>
2019-07-22 11:01:33 -0700

    EDGEMS-214 Fix to getNode

--- src/models/driveModel.js
+++ src/models/driveModel.js
@@ -1,5 +1,5 @@
 import Action from 'action-js';
-import { keyBy, mergeWith, values, find } from 'lodash';
+import { find } from 'lodash';
 import { extractToken } from '../edge-ms-helper/authorization-helper';
 import NodesMapper from '../helper/nodes-mapper';
 
@@ -34,45 +34,18 @@ function makeDriveModel(context, authorization) {
     .next(proximity => NodesMapper.transformMdsNodes(proximity.nodes, null, serviceType));
   }
 
-  function findMine() {
-    const nearby = findNearby();
-    const account = new Action((cb) => {
-      edge.clusterDiscovery('account', accessToken, nodes => cb(nodes), err => cb(err));
-    })
-    .next(data => Action.wrap({
-      accountId: data.accountId,
-      devices: NodesMapper.transformMdsNodes(data.nodes, null, serviceType),
-    }));
-
-    return Action.parallel([nearby, account], true)
-      .next((data) => {
-        const foundNearby = data[0];
-        const foundAccount = data[1].devices;
-        const accountId = data[1].accountId;
-
-        const nearbyNodes = keyBy(foundNearby.filter(node => node.accountId === accountId), 'id');
-        const accountNodes = keyBy(foundAccount, 'id');
-        return values(mergeWith(nearbyNodes, accountNodes, oldVal => oldVal));
-      });
-  }
-
   function findById(nodeId) {
-    return Action.parallel([findNearby(), findMine(), findProximity()], true)
+    return Action.parallel([findNearby(), findProximity()])
       .next(data => (
         find(data[0], device => (device.id === nodeId)) ||
-        find(data[1], device => (device.id === nodeId)) ||
-        find(data[2], device => (device.id === nodeId))
+        find(data[1], device => (device.id === nodeId))
       ))
-      .next((device) => {
-        if (!device) return new Error(`Cannot find device with id: ${nodeId}`);
-        return device;
-      });
+      .next(device => device || new Error(`Cannot find device with id: ${nodeId}`));
   }
 
   return {
     findNearby,
     findProximity,
-    findMine,
     findById,
   };
 }
